657b681e288989d6ffe637c932d9748cfb872067
Aina84 <johnsamuelaina84@gmail.com>
2025-04-08 15:51:12 +0100

    optimization token number

--- src/middlewares/messageFilter.mjs
+++ src/middlewares/messageFilter.mjs
@@ -1,10 +1,9 @@
-
 export function messageFilter(request,response,next){
     const { params : message } = request;
-    if(message.length>=50){
+    if(message.length>=20){
         return response.send({rep : ' votre message est trop long'})
     }else if(message.length==0){
         return response.send({reponse : 'veuillez entrer qulque chose'})
     }
-    next()
+    next();
 }   
